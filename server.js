f2f8eacf4041c94e860d67c2bdcee3e41e697d78
Adawari <adawarithomas@gmail.com>
2021-12-29 14:23:02 +0000

    fix port

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import * as path from "path";
 import registerRouter from "./routes/register.js";
 import authRouter from "./routes/auth.js";
@@ -11,8 +10,9 @@ import connectDB from "./config/db.js";
 const app = express();
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
-app.use(cors());
 app.use("/api/register", registerRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/search", searchRouter);
@@ -29,8 +29,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
